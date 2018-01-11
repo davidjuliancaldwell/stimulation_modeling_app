454533b8d609da24438b61a2895cc34e808472f9
David Caldwell <djcald@cs.washington.edu>
2018-01-10 17:14:15 -0800

    working with first verison of code

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import numpy as np
 import plotly as plotly
+import plotly.graph_objs as go
 import os
 
 app = dash.Dash(__name__)
@@ -10,20 +11,43 @@ server = app.server
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
+# bring in the data
+data_test = np.flipud(np.load('test_E.npy'))
+
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+    html.H2('Plots of Simulated Efield'),
+    dcc.Graph(id='contour_efield'),
+        dcc.Slider(
+            id='max_val',
+            min=0,
+            max=1000,
+            value=500,
+            step=None,
+            marks={i: '{}'.format(i) for i in range(0,1000,100)},
+        ),
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+@app.callback(
+    dash.dependencies.Output('contour_efield', 'figure'),
+    [dash.dependencies.Input('max_val', 'value')])
+def update_figure(selected_max):
+    maximum_val = int(selected_max)
+    return {
+        'data': [
+            #go.Contour([data])
+            go.Heatmap(
+                z=data_test,
+             colorscale='jet',
+             zauto = 'false',
+             zmin=0,
+             zmax=maximum_val)
+        ],
+        'layout': go.Layout(
+        xaxis={'title':'x dimension'},
+        yaxis={'title': 'depth'},
+        title='Plots of simulated field effects',
+        )
+    }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
