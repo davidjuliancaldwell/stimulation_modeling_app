dbcdf099f2bac0a139193dae28ffbb1b8a5b1755
David Caldwell <djcald@cs.washington.edu>
2018-01-14 21:07:05 -0800

    even less on HTTP

--- app.py
+++ app.py
@@ -195,8 +195,8 @@ app.layout = html.Div([
    [dash.dependencies.Input('button','n_clicks')],
    [dash.dependencies.State('load-data-box', 'value')])
 def clean_data(n_clicks,value):
-    k_min = [0,800]
-    k_max = [800,1001]
+    k_min = [0,900]
+    k_max = [900,1001]
     #k_min = [0,500]
     #k_max = [500,1001]
     Ex = np.zeros((1001,1001))
